42f46c46fe124688a3b2c548a385763591e57336
DiegoVanegas03 <gamesonfn@gmail.com>
2021-09-06 08:54:52 -0500

    Prueba del repositorio

---
+++ pregunta4.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+
+
+
+int main()
+{
+    int i=8,j=5,k;
+    float x=2.5, y=-0.3, z, resultado;
+    char a, b, c= 'c', d= 'd', r;
+
+   
+    r = islower(c);
+    printf("%c\n",r);
+   
+
+    system("pause");
+    return 0;
+
+}
+/*
+    r = isdigit(c);
+    printf("%c\n",r);
+
+    r = toupper(d);
+    printf("%c\n",r);
+
+    resultado = ceil(x);
+    printf("%f\n",resultado);
+
+    resultado = floor(x + y);
+    printf("%f\n",resultado);
+
+    r = islower(c);
+    printf("%c\n",r);
+
+    r = isupper(d);
+    printf("%c\n",r);
+
+    resultado = exp(i);
+    printf("%f\n",resultado);
+
+    resultado = log(j);
+    printf("%f\n",resultado);
+
+    resultado = sqrt(x * x + y * y);
+    printf("%f\n",resultado);
+
+    resultado = fmod(x, y);
+    printf("%f\n",resultado);
+
+    resultado = pow(x - y, 3.0);
+    printf("%f\n",resultado);
+
+    resultado = sin(x -y);
+    printf("%f\n",resultado);
+
+    resultado = strlen("Hola\0");
+    printf("%f\n",resultado);
+*/
